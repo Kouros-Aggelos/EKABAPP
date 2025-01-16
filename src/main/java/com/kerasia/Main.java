d17aa4d0fc2955c64b2d257c781fdaae790c5b6f
KonstantinaDimitraRigopoulou <t8230129@aueb.gr>
2025-01-16 02:44:38 +0200

    Update Main.java

--- src/main/java/com/kerasia/Main.java
+++ src/main/java/com/kerasia/Main.java
@@ -4,25 +4,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // 1) Δημιουργία Scanner
         Scanner scanner = new Scanner(System.in);
 
-        // 2) Μήνυμα καλωσορίσματος
+        /** Μήνυμα καλωσορίσματος!*/
         System.out.println("Καλώς ήρθατε στο σύστημα διαχείρισης ασθενών και εύρεσης νοσοκομείων!");
 
-        // 3) Δημιουργία χρήστη και συλλογή δεδομένων
         User user = new User(scanner); // Μεταβιβάζουμε το Scanner στον κατασκευαστή
         user.displayUserInfo();
 
-        // 4) Έλεγχος επιπέδου σοβαρότητας
         int severityLevel = user.getSeverityLevel();
 
-        // 5) Δημιουργία αντικειμένου hospitalfind
         hospitalfind hospitalFind = new hospitalfind();
 
         Closest closest = new Closest();
 
-        // 6) Διαχείριση ανάλογα με το επίπεδο σοβαρότητας
         if (severityLevel == 1 || severityLevel == 2 || severityLevel == 3) {
             handleLowSeverity(user, hospitalFind, closest);
         } else if (severityLevel == 4 || severityLevel == 5) {
@@ -32,7 +27,6 @@ public class Main {
             System.out.println("Μη έγκυρο επίπεδο σοβαρότητας.");
         }
 
-        // 7) Κλείσιμο του Scanner
         scanner.close();
 
         System.out.println("Άνοιγμα του χάρτη...");
@@ -41,7 +35,6 @@ public class Main {
     }
 
     public static void handleLowSeverity(User user, hospitalfind hospitalFind, Closest closest) {
-        // Παράδειγμα: Παίρνουμε νοσοκομεία για την Παθολογική
         String department = "Παθολογική";
         List<String[]> hospitals = hospitalFind.findHospitals(department, user.getDayOfWeek(), user.getTime());
 
@@ -52,11 +45,9 @@ public class Main {
 
     public static void handleHighSeverity(Scanner scanner, User user, hospitalfind hospitalFind, Suitable suitable) {
         
-        // Επιλογή τμήματος από τον χρήστη
         String selectedDepartment = suitable.selectDepartment(scanner);
         System.out.println("Επιλέχθηκε τμήμα: " + selectedDepartment);
 
-        // Αναζήτηση νοσοκομείων για το επιλεγμένο τμήμα
         List<String[]> hospitals = hospitalFind.findHospitals(selectedDepartment, user.getDayOfWeek(), user.getTime());
 
         if (hospitals.isEmpty()) {
@@ -69,30 +60,24 @@ public class Main {
     public static void findAndDisplayClosestHospital(User user, List<String[]> hospitals) {
         System.out.println("Υπολογισμός χρόνων διαδρομής προς τα νοσοκομεία...");
 
-        // Διεύθυνση χρήστη
         String userAddress = user.getAddress();
         String apiKey = "AIzaSyB5L0dTX88It7Qz8JZlQw72MKcF-i0uIJo";
 
-        // Δήλωση μεταβλητών για το κοντινότερο νοσοκομείο
         String closestHospital = null;
         String shortestDuration = null;
         int shortestDurationMinutes = Integer.MAX_VALUE;
 
-        // Υπολογισμός χρόνου διαδρομής για κάθε νοσοκομείο
         for (String[] hospital : hospitals) {
             String hospitalName = hospital[0];
             String hospitalAddress = hospital[1];
 
-            // Κλήση της μεθόδου calculateRouteTimes
             String duration = Frommap.calculateRouteTimes(userAddress, hospitalAddress, apiKey);
 
             if (duration != null) {
                 System.out.println("Χρόνος διαδρομής προς το νοσοκομείο " + hospitalName + ": " + duration);
 
-                // Μετατροπή της διάρκειας σε λεπτά
                 int durationInMinutes = parseDurationToMinutes(duration);
 
-                // Ενημέρωση για το κοντινότερο νοσοκομείο
                 if (durationInMinutes < shortestDurationMinutes) {
                     shortestDurationMinutes = durationInMinutes;
                     shortestDuration = duration;
@@ -102,8 +87,7 @@ public class Main {
                 System.out.println("Αποτυχία υπολογισμού διαδρομής για το νοσοκομείο " + hospitalName + ".");
             }
         }
-
-        // Εύρεση του κοντινότερου νοσοκομείου
+        
         if (closestHospital != null) {
             System.out.println("Το κοντινότερο νοσοκομείο είναι: " + closestHospital + " με χρόνο διαδρομής " + shortestDuration);
         } else {
